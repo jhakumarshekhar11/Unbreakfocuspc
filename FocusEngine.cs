66fb5aae60be3522a6fec589be33d41ac36d7d9e
Shekhar Kumar Jha <jhakumarshekhar11@gmail.com>
2026-03-31 06:58:38 +0000

    logo.ico and fix6

--- FocusEngine.cs
+++ FocusEngine.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Unbreakfocus.Desktop
+namespace Unbreakfocuspc
 {
     public class FocusEngine
     {
